7a7fda5217965f97a7ce8a9143a71f6bf2120e54
Tayyab Hussain <tayyab.hussain.it@gmail.com>
2022-09-06 01:02:09 +0500

    update gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  warehouseApiEndPoint: "https://programmingmentor.pk/warehouse-api/public/",
-  email: "tayayb_hussain_it@yahoo.com",
-  password: "1234567"
-};
